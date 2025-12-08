2e35b5d05d18923efb5dcc5c37cb8fe35fe6b678
suphakit085 <146973477+suphakit085@users.noreply.github.com>
2025-12-08 22:54:25 +0700

    add Main Program and Suphakit_4049

---
+++ Project/Suphakit_4049.md
@@ -0,0 +1,3 @@
+Author: ศุภกฤต แก้วแกมทอง 663380404-9
+Section: 3
+I am responsible for: Main Program
--- Project/project.py
+++ Project/project.py
@@ -1,1 +1,23 @@
-﻿
+﻿def main():
+    try:
+        input_num = input("ใส่ค่า Integer : ")
+        arr = list(map(int, input_num.split()))
+        print("เลือก Algorithm: 1 สำหรับ Bubble Sort, 2 สำหรับ Quick Sort")
+        algorithm = input("ใส่ตัวเลือกของคุณ: ").strip()
+
+        if algorithm == "1":
+            bubbleSort(arr)
+        elif algorithm == "2":
+            arr = quick_sort(arr)
+        else:
+            print("กรุณาเลือก 1 หรือ 2 เท่านั้น")
+            return
+
+        print("Sort array:", arr)
+    except ValueError:
+        print("กรุณาใส่ค่า Integer ให้ถูกต้อง")
+
+if __name__ == "__main__":
+    main()
+
+
